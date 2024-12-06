65cf4ce07394e58809d9680ebcdc4442e76ef6fd
Carlos Alvarez <carlosalvarez1391@gmail.com>
2024-12-06 23:35:10 +0100

    feat: using .env

--- src/firebase.ts
+++ src/firebase.ts
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCichl0Ww7Hs0j-Ko0q0FJLETeyzsvtE_Y",
-  authDomain: "timezone-converter-c658d.firebaseapp.com",
-  projectId: "timezone-converter-c658d",
-  storageBucket: "timezone-converter-c658d.firebasestorage.app",
-  messagingSenderId: "645257825142",
-  appId: "1:645257825142:web:d29af4534e3aa9d5558857",
-  measurementId: "G-HDE888W9PT"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
